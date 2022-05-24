2f1f615a1d18d7bbc3d4d95f702877987c668499
ErikNissen <73883328+ErikNissen@users.noreply.github.com>
2022-05-24 16:44:44 +0200

    update

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using static System.Math;
 using System.Collections.Generic;
@@ -18,6 +19,7 @@ public class CameraMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        AddCollider();
         Player1 = GameObject.Find("Player");
         Player2 = GameObject.Find("Player2");
         Middle_Vec = (Player1.transform.position + Player2.transform.position) / 2;
@@ -25,31 +27,32 @@ public class CameraMovement : MonoBehaviour
         MinFOV = Camera.main.fieldOfView;
     }
 
-    private void StopPlayerIfNotVisible(GameObject P)
+    // add collider to the camera
+    private void AddCollider()
     {
-        Vector3 player_pos = P.transform.position;
-        Rect field = Camera.main.rect;
-        float field_max_y = field.yMax;
-        float field_max_x = field.xMax;
-
-        if (player_pos.y > field_max_y)
+        BoxCollider collider = gameObject.AddComponent<BoxCollider>();
+        collider.isTrigger = true;
+        collider.size = new Vector3(Camera.main.orthographicSize * 2, Camera.main.orthographicSize * 2, 1);
+        collider.center = new Vector3(0, 0, 0);
+    }
+    
+    //stop player if at the edge of the screen
+    private void StopPlayer(GameObject Player)
+    {
+        Collider Camera_Collider = Camera.main.GetComponent<Collider>();
+        Collider Player_Collider = Player.GetComponent<Collider>();
+        
+        
+        //if player is at the edge of the screen
+        if (Player_Collider.bounds.max.x > Camera_Collider.bounds.max.x || Player_Collider.bounds.min.x < Camera_Collider.bounds.min.x ||
+            Player_Collider.bounds.max.y > Camera_Collider.bounds.max.y || Player_Collider.bounds.min.y < Camera_Collider.bounds.min.y)
         {
-            Vector3 new_pos;
-            new_pos.y = field_max_y;
-            new_pos.x = player_pos.x;
-            new_pos.z = player_pos.z;
-            P.transform.position = new_pos;
-        }
-
-        if (player_pos.x > field_max_x)
-        {
-            Vector3 new_pos;
-            new_pos.y = player_pos.y;
-            new_pos.x = field_max_x;
-            new_pos.z = player_pos.z;
-            P.transform.position = new_pos;
+            //change player x position by -1
+            Player.transform.position = new Vector3(Player.transform.position.x - 1, Player.transform.position.y, Player.transform.position.z);
         }
     }
+
+
     private float calcFOV(GameObject P1, GameObject P2)
     {
         float fov = 0f;
@@ -79,6 +82,10 @@ public class CameraMovement : MonoBehaviour
     {
         Player1 = GameObject.Find("Player");
         Player2 = GameObject.Find("Player2");
+        //create list of players
+        List<GameObject> Players = new List<GameObject>();
+        Players.Add(Player1);
+        Players.Add(Player2);
         
         Middle_Vec = (Player1.transform.position + Player2.transform.position) / 2;
         Middle_Vec.y += 1.5f;
@@ -88,15 +95,12 @@ public class CameraMovement : MonoBehaviour
         if (fov >= MinFOV && fov <= MaxFOV)
         {
             Camera.main.fieldOfView = fov;
-            if (Player1.transform.position.y > Camera.main.rect.yMax)
-            {
-                Player1.GetComponent<Rigidbody>().AddForce(new Vector2(0, Dropdown_speed), ForceMode.VelocityChange);
-            }
-            if (Player2.transform.position.y > Camera.main.rect.yMax)
-            {
-                Player2.GetComponent<Rigidbody>().AddForce(new Vector2(0, Dropdown_speed), ForceMode.VelocityChange);
-            }
         }
+        //iterate through players
+        /*foreach (var Player in Players)
+        {
+            StopPlayer(Player);
+        }*/
         /*float aspect = (
             Player1.transform.position.x > Player2.transform.position.x
                 ? Player1.transform.position.x - Player2.transform.position.x
