ebd5e7d590f77a66e2104648a5223f452d9d73d5
Alex <alexander.munkelt@student.fh-kiel.de>
2022-06-07 19:23:11 +0100

    CoffeeTweak v2

--- Assets/Scripts/ItemAnimation.cs
+++ Assets/Scripts/ItemAnimation.cs
@@ -9,13 +9,18 @@ public class ItemAnimation : MonoBehaviour
     [SerializeField]
     public float speed = 10f;
     [SerializeField]
-    public float moveLength = 0.1f;
+    public float moveHeight = 1f;
 
+    private Vector3 startPos;
+    private Vector3 endPos;
+
+    bool goUp = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPos = this.transform.position;
+        endPos = startPos + new Vector3(0, moveHeight, 0);
     }
 
     // Update is called once per frame
@@ -23,7 +28,24 @@ public class ItemAnimation : MonoBehaviour
     {
         this.transform.Rotate(0, Time.deltaTime * rotationSpeed, 0);
 
-        float y = Mathf.PingPong(Time.time * speed / 100, moveLength / 100) * 6;
-        this.transform.position = new Vector3(this.transform.position.x, y, this.transform.position.z);
+        //float y = Mathf.PingPong(Time.time * speed / 100, moveLength / 100) * 6;
+        //this.transform.position = new Vector3(this.transform.position.x, y, this.transform.position.z);
+
+        if (this.transform.position.y >= endPos.y - 0.01f && goUp)
+        {
+            goUp = false;
+        } else if (this.transform.position.y <= startPos.y + 0.01f && !goUp)
+        {
+            goUp = true;
+        }
+
+        if (goUp)
+        {
+            this.transform.position = Vector3.Lerp(this.transform.position, endPos, speed * Time.deltaTime);
+        }
+        else
+        {
+            this.transform.position = Vector3.Lerp(this.transform.position, startPos, speed * Time.deltaTime);
+        }
     }
 }
